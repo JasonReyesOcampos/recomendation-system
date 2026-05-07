9eee36564931a9cdfecb1f1d52777c2d0999d4d2
Jason9783 <reyesjason992@gmail.com>
2026-05-06 23:13:24 -0600

    config inicial

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        ; )
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
